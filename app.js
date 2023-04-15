374b5e583052213939fa06dad860633a035aca7b
osgt <129665060+osgt@users.noreply.github.com>
2023-04-15 18:22:11 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // app.js (Node.js + Express example)
 const express = require('express');
 const fs = require('fs');
+const crypto = require('crypto');
 const app = express();
 
 // Middleware to parse form data
@@ -10,12 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.post('/register', (req, res) => {
   const { name, email, bitcoinAddress } = req.body;
   const data = `Name: ${name}\nEmail: ${email}\nBitcoin Wallet Address: ${bitcoinAddress}\n\n`;
-  
-  // Append form data to a file
-  fs.appendFile('registrations.txt', data, (err) => {
-    if (err) throw err;
-    console.log('Form data saved!');
-  });
+
+  // Encrypt form data using AES-256-CBC with a secret key and IV
+  const secretKey = crypto.randomBytes(32);
+  const iv = crypto.randomBytes(16);
+  const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, iv);
+  const encryptedData = Buffer.concat([cipher.update(data, 'utf8'), cipher.final()]);
+
+  // Write encrypted data to a file
+  fs.writeFileSync('registration.enc', encryptedData);
+
+  // Write secret key and IV to separate files
+  fs.writeFileSync('secretKey.enc', secretKey);
+  fs.writeFileSync('iv.enc', iv);
 
   res.send('Registration successful!');
 });
