0c55689773c71e83d67fb290032849e968a61c7c
osgt <129665060+osgt@users.noreply.github.com>
2023-04-15 15:25:37 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,26 @@
-document.getElementById('registrationForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+// app.js (Node.js + Express example)
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+// Middleware to parse form data
+app.use(express.urlencoded({ extended: true }));
+
+// Handle form submissions
+app.post('/register', (req, res) => {
+  const { name, email, bitcoinAddress } = req.body;
+  const data = `Name: ${name}\nEmail: ${email}\nBitcoin Wallet Address: ${bitcoinAddress}\n\n`;
   
-    // Get form data
-    const name = document.getElementById('name').value;
-    const walletAddress = document.getElementById('walletAddress').value;
-    const email = document.getElementById('email').value;
-  
-    // Construct data string in CSV format
-    const dataString = `${name},${walletAddress},${email}\n`;
-  
-    // Use Fetch API to write data to a file
-    fetch('registrations.csv', {
-      method: 'POST',
-      body: dataString,
-      headers: {
-        'Content-Type': 'text/csv',
-        'Content-Disposition': 'attachment; filename=registrations.csv'
-      }
-    })
-    .then(function(response) {
-      if (response.ok) {
-        alert('Registration data saved successfully!');
-      } else {
-        alert('Error saving registration data.');
-      }
-    })
-    .catch(function(error) {
-      console.error('Error:', error);
-      alert('Error saving registration data.');
-    });
+  // Append form data to a file
+  fs.appendFile('registrations.txt', data, (err) => {
+    if (err) throw err;
+    console.log('Form data saved!');
   });
-  
+
+  res.send('Registration successful!');
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
